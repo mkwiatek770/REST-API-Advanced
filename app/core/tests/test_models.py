424759bd4ea014c0b88d17bce46d80a358815907
mkwiatek770 <michalkwiatek9@o2.pl>
2021-02-13 16:38:59 +0100

    Create new app core

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successfull(self):
+        """Test creating user with email is successful."""
+        email = 'some@gmail.com'
+        password = 'Testpass1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
