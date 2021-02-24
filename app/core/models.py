5ba9fd21c6c325b0afe4b358a79202e0f8499f24
mkwiatek770 <michalkwiatek9@o2.pl>
2021-02-24 09:44:02 +0100

    Add image field to Recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,6 @@
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -5,6 +8,16 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+RECIPE_PATH = 'uploads/recipe/'
+
+def recipe_image_file_path(instance, filename) -> str:
+    """Generate file path for new image."""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join(RECIPE_PATH, filename)
 
 
 class UserManager(BaseUserManager):
@@ -67,6 +80,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('core.Ingredient')
     tags = models.ManyToManyField('core.Tag')
+    image = models.ImageField(null=True, blank=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
